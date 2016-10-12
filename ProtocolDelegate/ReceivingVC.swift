412b6089c5107b0e1761f66ce3338ca2c1f8faad
Gustavo I. Chalco <gustavo.chalco@gmc-soft.com>
2016-10-11 23:42:50 -0500

    Creando controladores para hacer uso de protocolos

--- ProtocolDelegate/ReceivingVC.swift
+++ ProtocolDelegate/ReceivingVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ReceivingVC.swift
 //  ProtocolDelegate
 //
 //  Created by Gustavo on 10/11/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ReceivingVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
